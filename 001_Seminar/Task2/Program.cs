6c5b553371aabcedbf34fc37bb4d0e0c0cc5dd95
Ilya Sayenko <ilyasin060495@gmail.com>
2022-08-21 22:48:59 +0300

    tasks for seminar 1

---
+++ 001_Seminar/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+int max = a > b ? a : b;
+
+Console.WriteLine("max = " + max);
---
+++ 001_Seminar/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a > b ? a : b;
+max = max > c ? max: c;
+
+Console.WriteLine("max = " + max);
