06f54bb699190ddf641544bf17be8023e5d58433
Ilya Sayenko <ilyasin060495@gmail.com>
2022-09-02 17:37:23 +0300

    tasks for seminar 5

---
+++ 005_Seminar/Task36/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] createArray(int length) {
+    int[] array = new int [length];
+
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+int getSumOddPos(int[] array) {
+    int result = 0;
+    for (int i = 1; i < array.Length; i+=2) {
+        result += array[i];
+    } 
+    return result;
+}
+
+int[] array = createArray(5);
+System.Console.Write($"[{string.Join(", ", array)}] - > ");
+System.Console.WriteLine(getSumOddPos(array));
+System.Console.WriteLine(getSumOddPos(new int[]{3, 7, 23, 12}));
+System.Console.WriteLine(getSumOddPos(new int[]{-4, -6, 89, 6}));
---
+++ 005_Seminar/Task38/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76
+*/
+
+double[] createArray(int length) {
+    double[] array = new double [length];
+
+    for (int i = 0; i < array.Length; i++) {
+        array[i] = new Random().NextDouble();
+    }
+    return array;
+}
+
+double getMaxMinDiff(double[] array) {
+    double max = array[0];
+    double min = array[0];
+
+    foreach (double element in array) {
+        if (max < element) max = element;
+        if (min > element) min = element;
+    }
+    return Math.Abs(max - min);
+}
+
+double[] array = createArray(5);
+System.Console.Write($"[{string.Join(", ", array)}] -> ");
+System.Console.WriteLine(getMaxMinDiff(array));
+System.Console.WriteLine(getMaxMinDiff(new double[]{3, 7, 22, 2, 78}));
