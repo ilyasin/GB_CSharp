1994cc26e7fd0d392da597580c8b2e10b81e259b
Ilya Sayenko <ilyasin060495@gmail.com>
2022-09-05 22:55:35 +0300

    tasks for seminar 6

---
+++ 006_Seminar/Task41/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+void Task41() {
+    string input = Console.ReadLine();
+    int count = 0;
+    while (input != "stop") {
+        if (int.Parse(input) > 0) count++;
+        input = Console.ReadLine();
+    }
+    Console.WriteLine(count);
+}
+
+Task41();
